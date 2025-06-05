3efe8cddbb8a7e73d2f8abe837cfb591d60a1d34
Aayushi Shukla <53366218+AayushiShukla7@users.noreply.github.com>
2025-06-05 16:18:41 -0400

    Tailwind CSS added

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+  //title = 'My Angular eCommerce App';
+  title = signal('My Angular eCommerce App');
 
 }
