9fa4a3cd4bb94585fa2b1c7293c1c987a2ab229e
Aayushi Shukla <53366218+AayushiShukla7@users.noreply.github.com>
2025-06-09 15:31:41 -0400

    Stripe integration started

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -1,9 +1,10 @@
-import { Component, computed, inject } from '@angular/core';
+import { Component, computed, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { PrimaryButtonComponent } from '../../components/primary-button/primary-button.component';
 import { Customer } from '../../models/customer.model';
 import { Payment } from '../../models/payment.model';
 import { CartService } from '../../services/cart.service';
+import { environment } from '../../../environments/environment.development';
 
 @Component({
   selector: 'app-checkout',
@@ -14,13 +15,19 @@ import { CartService } from '../../services/cart.service';
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.css'
 })
-export class CheckoutComponent {
-
+export class CheckoutComponent implements OnInit {
+  
   customer: Customer = new Customer();
   payment: Payment = new Payment();
 
   cartService = inject(CartService);
   totalItemCount = this.cartService.cart().length;
+  cartItems = this.cartService.cart();
+  stripe = null;
+
+  ngOnInit(): void {
+    this.invokeStripe();
+  }
 
   total = computed(() => {
     let total = 0;
@@ -31,5 +38,19 @@ export class CheckoutComponent {
 
     return total;
   });
+
+  // Stripe Invocation
+  invokeStripe() {
+  if (!window.document.getElementById('stripe-script')) {
+    const script = window.document.createElement('script');
+    script.id = 'stripe-script';
+    script.type = 'text/javascript';
+    script.src = 'https://js.stripe.com/v3/';
+    script.onload = () => {
+      this.stripe = (<any>window).Stripe(environment.STRIPE_KEY);
+     };
+     window.document.body.appendChild(script);
+    }
+  }
 
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    STRIPE_KEY: "sk_test_51RY8yoPGKU13UoaT623rTJglblIJem0mBpYXXsDUw9tPKUpp2V7DoLJ9dwIz7VpAvBmfvt89Rotdf3HcoSMrPQ0A00tdPvw5Ng"
+};
