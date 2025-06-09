ec8a5fca2b183b19e3995dac1f60dd26c48639ab
Aayushi Shukla <53366218+AayushiShukla7@users.noreply.github.com>
2025-06-09 12:50:46 -0400

    Template form mapping and UI upgrades.

---
+++ src/app/models/payment.model.ts
@@ -0,0 +1,7 @@
+export class Payment {
+    nameOnCard: string = "";
+    ccNumber: string = "";
+    expMonth: string = "";
+    expYear: string = "";
+    cvvNumber: number = 0;
+}
--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { PrimaryButtonComponent } from '../../components/primary-button/primary-button.component';
+import { Customer } from '../../models/customer.model';
+import { Payment } from '../../models/payment.model';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-checkout',
@@ -12,5 +15,21 @@ import { PrimaryButtonComponent } from '../../components/primary-button/primary-
   styleUrl: './checkout.component.css'
 })
 export class CheckoutComponent {
+
+  customer: Customer = new Customer();
+  payment: Payment = new Payment();
+
+  cartService = inject(CartService);
+  totalItemCount = this.cartService.cart().length;
+
+  total = computed(() => {
+    let total = 0;
+
+    for(const item of this.cartService.cart()) {
+      total += item.price;
+    }
+
+    return total;
+  });
 
 }
